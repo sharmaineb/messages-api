ab4a2465897aa616352b2caf9074b6e2719928c3
Sharmaine <sharmaine.borbe@students.dominican.edu>
2023-04-20 14:14:46 -0700

    [update]

--- src/models/a24.js
+++ src/models/a24.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const MovieSchema = new Schema({
+const a24Schema = new Schema({
     title: { type: String, required: true},
     director: {type: String, required: true},
     yearRelease: {type: Number},
@@ -9,6 +9,6 @@ const MovieSchema = new Schema({
     actor: {type: String}
 });
 
-const Movie = mongoose.model('Movie', MovieSchema)
+const Movie = mongoose.model('Movie', a24Schema)
 
 module.exports = Movie
